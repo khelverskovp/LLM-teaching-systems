3b7c0caada7b8c7f7d9e3ddab07fd1caaa3a1eca
khelverskovp <s203294@student.dtu.dk>
2023-10-31 13:19:12 +0100

    Initial commit of cookiecutter project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Large language models for teaching-learning systems',
+    author='Kasper Helverskov Petersen',
+    license='MIT',
+)
